a6607431d2141928f48c5fad756a32c6239425c8
denis <182906@unsaac.edu.pe>
2022-03-20 22:31:14 -0500

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="flex h-screen justify-center items-center font-[Outfit]">
         <div className="w-[22%] h-[550px] bg-[#FFFFFF] rounded-[20px] flex flex-col p-[1.2%] justify-around">
-          <img src={qrImage} alt="QR image" className="rounded-md" />
+          <img src={qrImage} alt="QR" className="rounded-md" />
           <span className="block mt-4 font-bold text-center text-[24px] mb-4">Improve your front-end skills by building projects</span>
           <p className="block text-[17px] m-[7px] font-normal text-gray-600/80 text-center leading-4">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
