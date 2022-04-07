3010f029e6d1945ffa978615c5bc40ed783edd9d
denis <182906@unsaac.edu.pe>
2022-04-07 10:25:37 -0500

    Clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import qrImage from "./assets/image-qr-code.png";
+import QR from "./assets/image-qr-code.png";
 
 function App() {
   return (
     <>
       <div className="flex h-screen justify-center items-center font-[Outfit]">
         <div className="w-[22%] h-[550px] bg-[#FFFFFF] rounded-[20px] flex flex-col p-[1.2%] justify-around">
-          <img src={qrImage} alt="QR" className="rounded-md" />
-          <span className="block mt-4 font-bold text-center text-[24px] mb-4">Improve your front-end skills by building projects</span>
-          <p className="block text-[17px] m-[7px] font-normal text-gray-600/80 text-center leading-4">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+          <img src={QR} alt="QR" className="rounded-md" />
+          <span className="block mt-4 font-bold text-center text-[24px] mb-4">
+            Improve your front-end skills by building projects
+          </span>
+          <p className="block text-[17px] m-[7px] font-normal text-gray-600/80 text-center leading-4">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
         </div>
       </div>
     </>
