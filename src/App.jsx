8e773cac1bf8cfb7cce65a82f992064a038d0cec
denis <182906@unsaac.edu.pe>
2022-04-07 11:35:54 -0500

    Add mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import QR from "./assets/image-qr-code.png";
 function App() {
   return (
     <>
-      <div className="flex h-screen justify-center items-center font-[Outfit]">
-        <div className="w-[22%] h-[550px] bg-[#FFFFFF] rounded-[20px] flex flex-col p-[1.2%] justify-around">
-          <img src={QR} alt="QR" className="rounded-md" />
-          <span className="block mt-4 font-bold text-center text-[24px] mb-4">
+      <div className="flex min-h-screen justify-center items-center font-[Outfit]">
+        <div className="w-[320px] h-[495px] bg-[#FFFFFF] rounded-2xl flex flex-col pt-[14px] items-center">
+          <img src={QR} alt="QR" className="rounded-xl w-[290px] h-[290px]" />
+          <span className="block font-bold text-center text-[1.5rem] leading-[30px] tracking-[-0.07rem] mt-[20px] mx-[20px]">
             Improve your front-end skills by building projects
           </span>
-          <p className="block text-[17px] m-[7px] font-normal text-gray-600/80 text-center leading-4">
+          <p className="block text-[1rem] m-[7px] tracking-[-0.02rem] font-normal text-gray-600/80 text-center leading-5 mx-[40px] mt-[15px]">
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level
           </p>
