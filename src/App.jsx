dd68a96820b0ba8b75b5b888a5772f9539f6ee52
denis <182906@unsaac.edu.pe>
2022-04-07 12:22:48 -0500

    Add min weigth

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import QR from "./assets/image-qr-code.png";
 function App() {
   return (
     <>
-      <div className="flex min-h-screen items-center justify-center font-[Outfit]">
+      <div className="flex min-h-screen min-w-[350px] items-center justify-center px-[20px] font-[Outfit]">
         <div className="flex h-[495px] w-[320px] flex-col items-center rounded-2xl bg-[#FFFFFF] pt-[14px]">
           <img src={QR} alt="QR" className="h-[290px] w-[290px] rounded-xl" />
           <span className="mx-[20px] mt-[20px] block text-center text-[1.5rem] font-bold leading-[30px] tracking-[-0.07rem] text-[#1F3150]">
