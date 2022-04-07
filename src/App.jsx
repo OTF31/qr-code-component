cc1cb7db4b24491c2174c285daf475a4e1f99791
denis <182906@unsaac.edu.pe>
2022-04-07 12:07:30 -0500

    Add prettier dependency

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import QR from "./assets/image-qr-code.png";
 function App() {
   return (
     <>
-      <div className="flex min-h-screen justify-center items-center font-[Outfit]">
-        <div className="w-[320px] h-[495px] bg-[#FFFFFF] rounded-2xl flex flex-col pt-[14px] items-center">
-          <img src={QR} alt="QR" className="rounded-xl w-[290px] h-[290px]" />
-          <span className="block font-bold text-center text-[1.5rem] leading-[30px] tracking-[-0.07rem] mt-[20px] mx-[20px] text-[#1F3150]">
+      <div className="flex min-h-screen items-center justify-center font-[Outfit]">
+        <div className="flex h-[495px] w-[320px] flex-col items-center rounded-2xl bg-[#FFFFFF] pt-[14px]">
+          <img src={QR} alt="QR" className="h-[290px] w-[290px] rounded-xl" />
+          <span className="mx-[20px] mt-[20px] block text-center text-[1.5rem] font-bold leading-[30px] tracking-[-0.07rem] text-[#1F3150]">
             Improve your front-end skills by building projects
           </span>
-          <p className="block text-[1rem] m-[7px] tracking-[-0.02rem] font-normal text-gray-600/80 text-center leading-5 mx-[40px] mt-[15px]">
+          <p className="m-[7px] mx-[40px] mt-[15px] block text-center text-[1rem] font-normal leading-5 tracking-[-0.02rem] text-gray-600/80">
             Scan the QR code to visit Frontend Mentor and take your coding
             skills to the next level
           </p>
