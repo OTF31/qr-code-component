f339043b82beb3beba1b523c9d95a5f14b5f1623
denis <182906@unsaac.edu.pe>
2022-04-07 12:06:30 -0500

    Add text color

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <div className="flex min-h-screen justify-center items-center font-[Outfit]">
         <div className="w-[320px] h-[495px] bg-[#FFFFFF] rounded-2xl flex flex-col pt-[14px] items-center">
           <img src={QR} alt="QR" className="rounded-xl w-[290px] h-[290px]" />
-          <span className="block font-bold text-center text-[1.5rem] leading-[30px] tracking-[-0.07rem] mt-[20px] mx-[20px]">
+          <span className="block font-bold text-center text-[1.5rem] leading-[30px] tracking-[-0.07rem] mt-[20px] mx-[20px] text-[#1F3150]">
             Improve your front-end skills by building projects
           </span>
           <p className="block text-[1rem] m-[7px] tracking-[-0.02rem] font-normal text-gray-600/80 text-center leading-5 mx-[40px] mt-[15px]">
